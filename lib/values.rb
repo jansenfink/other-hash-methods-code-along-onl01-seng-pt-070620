3f76e7f74bc993cc6364c9e8360acb6c3de9a9df
Jansen Fink <jansenfink@charter.net>
2020-07-19 01:10:23 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -9,7 +9,9 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-groceries.values
+  #code your solution here!
+  groceries.values 
+  groceries.values.flatten 
 end
 
-get_the_values
+get_the_values(groceries)
